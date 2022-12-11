e44c226de46161337695cf867e307ff42114202b
GETACUSER-PC\GETAC USER <ninov_16@yahoo.com>
2022-12-11 14:08:47 +0100

    first commit

---
+++ controllers/bookController.js
@@ -0,0 +1,131 @@
+const {bookModel, userModel} = require('../models');
+
+function newBook(isbn, title, authors, published, subtitle, rating, thumbnails, userId, description) {
+    return bookModel.create({isbn, title, authors, published, subtitle, rating, thumbnails, userId, description })
+        .then(book => {
+            console.log(thumbnails);
+            return Promise.all([
+                userModel.updateOne({ _id: userId }, { $push: { books: book._id } }),
+                //thumbnailModel.findByIdAndUpdate({ _id: thumbnailId },{ $push: { thumbnailId: thumbnailId}}, { new: true }),
+
+            ])
+        })
+}
+
+
+function getAllBooks(req, res, next) {
+    const { _id: userId } = req.user;
+    bookModel.find({userId})
+        //.sort({ created_at: -1 })
+        //.populate('thumbnailId userId')
+        .then(books => {
+            res.status(200).json(books)
+        })
+        .catch(next);
+}
+
+function searchBooks(req, res, next){
+    const { _id: userId } = req.user;
+    const { searchTerm } = req.params;
+     
+    const searchResult = new RegExp(searchTerm, 'i');
+
+    bookModel.find({userId}).or([{'title': {$regex: searchResult}}, {'subtitel': {$regex: searchResult}}, {'authors': {$regex: searchResult}}]).lean()
+    .then(books => {
+        res.status(200).json(books)
+    })
+    .catch(next);
+}
+
+
+function createBook(req, res, next) {
+    const { thumbnails } = req.body;
+    const { _id: userId } = req.user;
+    const { title } = req.body;
+    const { isbn } = req.body;
+    const { authors } = req.body;
+    const { published } = req.body;
+    const { subtitle } = req.body;
+    const { rating } = req.body;
+    const { description } = req.body;
+
+    console.log("------------")
+    console.log(thumbnails);
+
+    newBook(isbn, title, authors, published, subtitle, rating, thumbnails, userId, description)
+        .then(([_, updatedBook]) => res.status(200).json(updatedBook))
+        .catch(next);
+        
+}
+
+function getBook(req, res, next) {
+    const { bookId } = req.params;
+
+    bookModel.findById(bookId)
+        .populate({
+            path : 'userId'
+          })
+        .then(book => res.json(book))
+        .catch(next);
+}
+
+function editBook(req, res, next){
+    const { _id: bookId } = req.body;
+    const { thumbnails } = req.body;
+    const { _id: userId } = req.user;
+    const { title } = req.body;
+    const { isbn } = req.body;
+    const { authors } = req.body;
+    const { published } = req.body;
+    const { subtitle } = req.body;
+    const { rating } = req.body;
+    const { description } = req.body;
+
+
+    bookModel.findOneAndUpdate({ _id: bookId, userId }, {isbn, title, authors, published, subtitle, rating, thumbnails, description}, { new: true })
+    .then(updatedBook => {
+        if (updatedBook) {
+            res.status(200).json(updatedBook);
+        }
+        else {
+            res.status(401).json({ message: `Not allowed! to edit the page` });
+        }
+    })
+    .catch(next);
+
+}
+
+function deleteBook(req, res, next) {
+    const { bookId } = req.params;
+    const { _id: userId } = req.user;
+
+    console.log('userId ' + userId)
+    console.log('bookId ' + bookId)
+    console.log(req.params);
+ 
+
+
+    Promise.all([
+        bookModel.findOneAndDelete({ _id: bookId, userId }),
+        userModel.findOneAndUpdate({ _id: userId }, { $pull: { books: bookId } }),
+
+    ])
+        .then(([deletedOne, _, __]) => {
+            if (deletedOne) {
+                res.status(200).json(deletedOne)
+            } else {
+                res.status(401).json({ message: `Not allowed! to edit the page` });
+            }
+        })
+        .catch(next);
+}
+
+
+module.exports = {
+    getAllBooks,
+    createBook,
+    getBook,
+    editBook,
+    deleteBook,
+    searchBooks
+}
---
+++ router/books.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../utils');
+const { bookController } = require('../controllers');
+
+// middleware that is specific to this router
+
+router.get('/',  auth(), bookController.getAllBooks);
+router.post('/', auth(), bookController.createBook);
+
+
+router.get('/catalog/:bookId', auth(), bookController.getBook);
+
+router.put('/edit/:bookId', auth(), bookController.editBook);
+router.delete('/delete/:bookId', auth(), bookController.deleteBook);
+router.get('/search/:searchTerm', auth(), bookController.searchBooks);
+
+module.exports = router
